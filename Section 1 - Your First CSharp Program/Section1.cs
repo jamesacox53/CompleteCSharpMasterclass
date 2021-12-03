e30c96d50d2ea916fbc0fc9173bb3157e8830987
jamesacox53 <jamesacox53@gmail.com>
2021-12-03 21:42:49 +0000

    Completed Exercise: Strings 2

--- Section 1 - Your First CSharp Program/Section1.cs
+++ Section 1 - Your First CSharp Program/Section1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-namespace HelloWorld
+namespace Section1Namespace
 {
-    class Program
+    class Section1
     {
         static void Main(string[] args)
         {
---
+++ Section 2 - DataTypes and Variables/Section2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Section2Namespace
+{
+    class Section2
+    {
+
+        static void Main(string[] args)
+        {
+            // Section2.ExerciseStrings1();
+            Section2.ExerciseStrings2();
+        }
+
+        public static void ExerciseStrings1()
+        {
+            string value;
+
+            Console.WriteLine("Please enter your name and press enter");
+
+            string returnedValue = Console.ReadLine();
+
+            value = returnedValue;
+            Console.WriteLine($"Upper Case                :{value.ToUpper()}");
+            Console.WriteLine($"Lower Case                :{value.ToLower()}");
+            Console.WriteLine($"Trimmed Value             :{value.Trim()}");
+            Console.WriteLine($"Substring from position 3 :{value.Substring(3)}");
+
+        }
+
+        public static void ExerciseStrings2()
+        {
+            Console.WriteLine("Enter a string here:");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Enter the character to search:");
+            char inputCharacter = Console.ReadLine()[0];
+            Console.WriteLine($"Index of first occurrence: {inputString.IndexOf(inputCharacter)}");
+
+            Console.WriteLine("Please Input your first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please Input your surname:");
+            string surname = Console.ReadLine();
+            string fullName = string.Concat(firstName, " ", surname);
+            Console.WriteLine($"Your full name is: {fullName}");
+
+        }
+    
+    }
+
+}
