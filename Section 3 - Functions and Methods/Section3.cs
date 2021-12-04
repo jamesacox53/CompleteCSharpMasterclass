7c16596c6c3737f586f2b8877aa0637ab86007e4
jamesacox53 <jamesacox53@gmail.com>
2021-12-04 13:17:43 +0000

    Completed Challenge - Methods

---
+++ Section 3 - Functions and Methods/Section3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Section3Namespace
+{
+    class Section3
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Billy";
+            string friend2 = "Joseph";
+            string friend3 = "Ben";
+
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+        }
+
+        public static void GreetFriend(string friendName)
+        {
+            Console.WriteLine($"Hi {friendName}, my friend!");
+        }
+
+    }
+
+}
