ac40872bce0978b0d0324b01a7f924945f2c3a79
jamesacox53 <jamesacox53@gmail.com>
2021-12-16 21:35:20 +0000

    Completed Introduction To WPF - And When To Use It

---
+++ Section 13 - WPF - Windows Presentation Foundation/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Section_13___WPF___Windows_Presentation_Foundation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
