d0c74133134abafa18aecb49e0507c89d0b1bd12
jamesacox53 <jamesacox53@gmail.com>
2021-12-21 21:13:46 +0000

    Completed ListBox and a List of Current Matches

--- Section 13 - WPF - Windows Presentation Foundation/MainWindow.xaml.cs
+++ Section 13 - WPF - Windows Presentation Foundation/MainWindow.xaml.cs
@@ -23,6 +23,45 @@ namespace Section_13___WPF___Windows_Presentation_Foundation
         public MainWindow()
         {
             InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match(3, 2, "Bayern Munich", "Real Madrid", 85));
+            matches.Add(new Match(4, 2, "Bayern Munich", "Barcelona",75));
+            matches.Add(new Match(1, 0, "Tottenham Hotspur", "Newcastle United", 30));
+
+            lbMatches.ItemsSource = matches;
         }
+
+        private void ShowSelectedButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected Match: " + lbMatches.SelectedItem.ToString());
+            }
+        }
+    }
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+
+        public int Completion { get; set; }
+
+        public Match(int score1, int score2, string team1, string team2, int completion)
+        {
+            this.Score1 = score1;
+            this.Score2 = score2;
+            this.Team1 = team1;
+            this.Team2 = team2;
+            this.Completion = completion;
+        }
+
+        public override string ToString()
+        {
+            return $"{Team1} {Score1} : {Score2} {Team2}";
+        }
+
     }
 }
